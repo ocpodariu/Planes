da20bae9d15b2491d77cb3666a9a3537dfa8200a
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-02-12 22:49:02 +0200

    Define constants for board size

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -8,6 +8,9 @@ public class Constants {
 	
 	public static final int GAME_ATTACK_INVALID = -1;
 	public static final int GAME_ATTACK_MISS    =  0;
-	public static final int GAME_ATTACK_HIT     =  1;	
+	public static final int GAME_ATTACK_HIT     =  1;
+	
+	public static final int BOARD_WIDTH  = 8;
+	public static final int BOARD_HEIGHT = 8;
 
 }
